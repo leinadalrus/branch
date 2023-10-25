d9ceac39b864634c5c322e6cd3c2c721eccb277b
David <daniel.david.surla@gmail.com>
2023-10-25 17:24:02 +1100

    updated all exported default functions to act as proper components

--- app/navmenus/navmenu.js
+++ app/navmenus/navmenu.js
@@ -1,34 +1,28 @@
-import { Link } from 'next/link'
+import styles from './navmenu.module.css'
 
-export const NavMenu = () => {
+export function NavMenu() {
   return (
-    <>
-      <div class="FrostedBanner">
-        <div class="NavBackground"></div>
-        <nav class="Navbar">
-          <Link class="Overwatch" href="/">
-            <button>Home Base</button>
-          </Link>
+    <div class={styles.FrostedBanner}>
+      <div class={styles.NavBackground}></div>
+      <nav class={styles.Navbar}>
+        <a class={styles.Overwatch}>
+          <button>Home Base</button>
+        </a>
+        <a class={styles.Sortie}>
+          <button>Mission Sortie</button>
+        </a>
+        <a class={styles.Assembly}>
+          <button>My Garage</button>
+        </a>
+        <a class={styles.Mission}>
+          <button>NEST: Branch</button>
+        </a>
 
-          <Link class="Sortie" href="/#sortie">
-            <button>Mission Sortie</button>
-          </Link>
-
-          <Link class="Assembly" href="/#assembly">
-            <button>My Garage</button>
-          </Link>
-
-          <Link class="Mission" href="/#mission">
-            <button>NEST: Branch</button>
-          </Link>
-
-          <div id="Line1"></div>
-          <div id="Line2"></div>
-          <div id="Line3"></div>
-          <div id="Line4"></div>
-        </nav>
-      </div>
-    </>
+        <div class={styles.Line1}></div>
+        <div class={styles.Line2}></div>
+        <div class={styles.Line3}></div>
+        <div class={styles.Line4}></div>
+      </nav>
+    </div>
   )
 }
-
--- app/page.js
+++ app/page.js
@@ -1,43 +1,20 @@
-import Image from 'next/image'
+import { NavMenu } from './navmenus/navmenu'
 import styles from './page.module.css'
 
-const Index = () => {
+const Home = () => {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h1 class={styles.card} id="contentHeadingTitle">
+          B<hr />R A N<br />C H
+        </h1>
+        <h2 class={styles.description}>
+          <i>Main systems: nominal.</i>
+        </h2>
       </div>
+
+      <NavMenu />
 
       <div className={styles.grid}>
         <a
@@ -90,8 +67,15 @@ const Index = () => {
           </p>
         </a>
       </div>
+
+      <footer class={styles.description}>
+        <p>@<span>:</span><i>daniel.david.surla@gmail.com</i></p>
+        <i>Daniel David Sansano Surla</i>
+        <i>All Rights Reserved</i>
+      </footer>
+
     </main>
   )
 }
 
-export default Index
+export default Home
